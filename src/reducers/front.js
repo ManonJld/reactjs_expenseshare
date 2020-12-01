39fef5a8a3ed7275bf5613e426649235c8de8970
Manon <manon.jeuland@gmail.com>
2020-12-01 11:16:38 +0100

    installation of react-router, redux, redux thunk and node-sass

---
+++ src/reducers/front.js
@@ -0,0 +1,18 @@
+import {
+
+} from "../action/action"
+
+
+const initialState = {
+
+};
+
+function front(state = initialState, action) {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
+
+export default front;
